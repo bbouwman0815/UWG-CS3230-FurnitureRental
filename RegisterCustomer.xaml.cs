07719af1ebb4adaacb37e93b55bc3bfd779ce130
Austin Snyder <asnyder5@my.westga.edu>
2021-10-20 18:32:58 -0400

    Added validation

--- RegisterCustomer.xaml.cs
+++ RegisterCustomer.xaml.cs
@@ -1,20 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using System.Text.RegularExpressions;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using UWG_CS3230_FurnitureRental.Model;
 using UWG_CS3230_FurnitureRental.DAL;
+using UWG_CS3230_FurnitureRental.Model;
 
 // The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,15 +11,27 @@ namespace UWG_CS3230_FurnitureRental
 {
     public sealed partial class RegisterCustomer : ContentDialog
     {
-        ObservableCollection<String> states = new ObservableCollection<String>();
+        #region Data members
 
-        EmployeeDAL dal = new EmployeeDAL();
+        private const string phoneRegex = @"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$";
+        private const string zipRegex = @"^\d{5}-?(\d{4})?$";
+        private readonly ObservableCollection<string> states = new ObservableCollection<string>();
+
+        private readonly EmployeeDAL dal = new EmployeeDAL();
+
+        #endregion
+
+        #region Constructors
 
         public RegisterCustomer()
         {
             this.InitializeComponent();
             this.initStates();
         }
+
+        #endregion
+
+        #region Methods
 
         private void initStates()
         {
@@ -42,22 +43,68 @@ namespace UWG_CS3230_FurnitureRental
 
         private void ContentDialog_CancelButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
-            
         }
 
         private void ContentDialog_SaveButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
         {
-            var address = this.createAddress();
-            var newCustomer = this.createCustomer(address);
-            dal.CreateNewAddress(address);
-            var id = dal.GetAddressId(address);
-            dal.CreateNewCustomer(newCustomer, id);
+            if (string.IsNullOrEmpty(this.fNameTextBox.Text))
+            {
+                args.Cancel = true;
+                this.errorTextBox.Text = "First Name Required";
+            }
+            else if (string.IsNullOrEmpty(this.lNameTextBox.Text))
+            {
+                args.Cancel = true;
+                this.errorTextBox.Text = "Last Name Required";
+            }
+            else if (this.genderComboBox.SelectedValue == null)
+            {
+                args.Cancel = true;
+                this.errorTextBox.Text = "Gender Required";
+            }
+            else if (!Regex.IsMatch(this.phoneTextBox.Text, phoneRegex))
+            {
+                args.Cancel = true;
+                this.errorTextBox.Text = "Phone Format Invalid";
+            }
+            else if (this.bdayDatePicker.SelectedDate == null)
+            {
+                args.Cancel = true;
+                this.errorTextBox.Text = "Birthday Required";
+            }
+            else if (string.IsNullOrEmpty(this.addr1TextBox.Text))
+            {
+                args.Cancel = true;
+                this.errorTextBox.Text = "Address 1 Required";
+            }
+            else if (string.IsNullOrEmpty(this.cityTextBox.Text))
+            {
+                args.Cancel = true;
+                this.errorTextBox.Text = "City Required";
+            }
+            else if (this.stateComboBox.SelectedValue == null)
+            {
+                args.Cancel = true;
+                this.errorTextBox.Text = "State Required";
+            }
+            else if (!Regex.IsMatch(this.zipTextBox.Text, zipRegex))
+            {
+                args.Cancel = true;
+                this.errorTextBox.Text = "Zip Format Invalid";
+            }
+            else
+            {
+                var address = this.createAddress();
+                var newCustomer = this.createCustomer(address);
+                this.dal.CreateNewAddress(address);
+                var id = this.dal.GetAddressId(address);
+                this.dal.CreateNewCustomer(newCustomer, id);
+            }
         }
 
         private Address createAddress()
         {
-            Address customerAddress = new Address()
-            {
+            var customerAddress = new Address {
                 id = null,
                 address1 = this.addr1TextBox.Text,
                 address2 = this.addr2TextBox.Text,
@@ -70,8 +117,7 @@ namespace UWG_CS3230_FurnitureRental
 
         private Customer createCustomer(Address customerAddress)
         {
-            Customer newCustomer = new Customer()
-            {
+            var newCustomer = new Customer {
                 fName = this.fNameTextBox.Text,
                 lName = this.lNameTextBox.Text,
                 address = customerAddress,
@@ -82,5 +128,7 @@ namespace UWG_CS3230_FurnitureRental
             };
             return newCustomer;
         }
+
+        #endregion
     }
 }
